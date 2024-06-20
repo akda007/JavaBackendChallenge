260267da642186bc54aa07c87b9131c1cf267ed2
akda007 <akdagmr007s@gmail.com>
2024-06-20 16:00:23 -0300

    remove: unused imports

--- backend/src/test/java/com/trevis/startup/javaproject/AuthServiceTest.java
+++ backend/src/test/java/com/trevis/startup/javaproject/AuthServiceTest.java
@@ -2,7 +2,6 @@ package com.trevis.startup.javaproject;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
--- backend/src/test/java/com/trevis/startup/javaproject/DepartmentServiceTest.java
+++ backend/src/test/java/com/trevis/startup/javaproject/DepartmentServiceTest.java
@@ -2,7 +2,6 @@ package com.trevis.startup.javaproject;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
